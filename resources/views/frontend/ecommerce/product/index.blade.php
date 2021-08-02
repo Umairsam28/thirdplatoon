8567a37addb5c17db504ea7ea98c5a6b10adec35
Yrux <yruxwork@gmail.com>
2021-08-03 03:41:04 +0500

    fixes

--- resources/views/frontend/ecommerce/product/index.blade.php
+++ resources/views/frontend/ecommerce/product/index.blade.php
@@ -12,37 +12,39 @@
                 <h2>Categories</h2>
                 <ul class="category-select">
                     @foreach($categories as $category)
-                    <li>{{$category->name}}</li>
+                    <li><a href="">{{$category->name}}</a></li>
                     @endforeach
                 </ul>
-                <h2>MOST VIEWED</h2>
-                <ul class="small-product-card">
-                    @foreach($mostViewed as $mv)
-                    <li>
-                        @if(count($mv->images)>0)
-                        <img src="{{asset('storage/products/'.$mv->id.'/'.$mv->images[0]->image_path)}}" />
-                        @else
-                        <img src="assets/images/placeholder.png" />
-                        @endif
-                        <h4><a href="{{route('shop.detail',[$mv])}}">{{$mv->name}}</a></h4>
-                        <span>${{$mv->price}}</span>
-                    </li>
-                    @endforeach
-                </ul>
+                <!--<h2>MOST VIEWED</h2>-->
+                <!--<ul class="small-product-card">-->
+                <!--    @foreach($mostViewed as $mv)-->
+                <!--    <li>-->
+                <!--        @if(count($mv->images)>0)-->
+                <!--        <img src="{{asset('storage/products/'.$mv->id.'/'.$mv->images[0]->image_path)}}" />-->
+                <!--        @else-->
+                <!--        <img src="assets/images/placeholder.png" />-->
+                <!--        @endif-->
+                <!--        <h4><a href="{{route('shop.detail',[$mv])}}">{{$mv->name}}</a></h4>-->
+                <!--        <span>${{$mv->price}}</span>-->
+                <!--    </li>-->
+                <!--    @endforeach-->
+                <!--</ul>-->
             </div>
             <div class="col-md-9">
                 <div class="row products_list">
                     @foreach($products as $product)
                     <div class="col-md-4 product-card">
-                        <a href="{{route('shop.detail',[$product])}}">
-                            @if(count($product->images)>0)
-                            <img src="{{asset('storage/products/'.$product->id.'/'.$product->images[0]->image_path)}}" />
-                            @else
-                            <img src="assets/images/placeholder.png" />
-                            @endif
-                            <h4>{{$product->name}}</h4>
-                            <h5>${{$product->price}}</h5>
-                        </a>
+                        <div class="products_box">
+                            <a href="{{route('shop.detail',[$product])}}">
+                                @if(count($product->images)>0)
+                                <img src="{{asset('storage/products/'.$product->id.'/'.$product->images[0]->image_path)}}" />
+                                @else
+                                <img src="assets/images/placeholder.png" />
+                                @endif
+                                <h4>{{$product->name}}</h4>
+                                <h5>${{$product->price}}</h5>
+                            </a>
+                        </div>
                     </div>
                     @endforeach
                 </div>
